baae4c78282f185a30e61b3b04d1782046f61dd7
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-09-13 21:19:46 +0545

    creating button component

---
+++ frontend/components/custom/button.tsx
@@ -0,0 +1,90 @@
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+type ButtonProps = {
+  backgroundColor?: string;
+
+  height?: number | string;
+  width?: number | string;
+
+  padding?: number | string;
+  paddingVertical?: number | string;
+  paddingHorizontal?: number | string;
+
+  margin?: number | string;
+  marginVertical?: number | string;
+  marginHorizontal?: number | string;
+  marginTop?: number | string;
+  marginBottom: number | string;
+
+  borderWidth?: number;
+  borderColor?: string;
+  borderStyle?: string;
+  borderRadius?: number;
+
+  color?: string;
+  fontSize?: number;
+  fontWeight?: number;
+};
+
+type NavButtonProps = {
+  buttonInfo: ButtonProps;
+  text: string;
+  link?: string;
+  onPress?: () => void;
+};
+
+export function NavButton({ buttonInfo, link, text, onPress }: NavButtonProps) {
+  const styles = createStyle(buttonInfo);
+  if (link) {
+    // navigation version
+    return (
+      <Link href={link} asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>{text}</Text>
+        </Pressable>
+      </Link>
+    );
+  }
+
+  // non-navigation version
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </Pressable>
+  );
+}
+function createStyle(buttonInfo: ButtonProps["buttonInfo"]) {
+  return StyleSheet.create({
+    button: {
+      backgroundColor: buttonInfo.backgroundColor ?? "#FFAE42",
+
+      justifyContent: "center",
+      alignItems: "center",
+
+      alignSelf: "flex-start",
+      width: buttonInfo.width ?? "auto",
+      height: buttonInfo.height ?? "auto",
+
+      padding: buttonInfo.padding ?? 0,
+      paddingHorizontal: buttonInfo.paddingHorizontal ?? 0,
+      paddingVertical: buttonInfo.paddingVertical ?? 0,
+
+      margin: buttonInfo.margin ?? 0,
+      marginHorizontal: buttonInfo.marginHorizontal ?? 0,
+      marginVertical: buttonInfo.marginVertical ?? 0,
+      marginTop: buttonInfo.marginTop ?? 0,
+      marginBottom: buttonInfo.marginBottom ?? 0,
+
+      borderRadius: buttonInfo.borderRadius ?? 10,
+      borderWidth: buttonInfo.borderWidth ?? 1,
+      borderStyle: buttonInfo.borderStyle ?? "solid",
+      borderColor: buttonInfo.borderColor ?? "black",
+    },
+    buttonText: {
+      color: buttonInfo.color ?? "black",
+      fontSize: buttonInfo.fontSize ?? 24,
+      fontWeight: buttonInfo.fontWeight ?? 900,
+    },
+  });
+}
