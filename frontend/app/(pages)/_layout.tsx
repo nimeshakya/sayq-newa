e5f70381c34fdec950aac4b15fd54224751942a6
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-09-19 16:26:42 +0545

    adding initial setup for redux

--- frontend/app/(pages)/_layout.tsx
+++ frontend/app/(pages)/_layout.tsx
@@ -1,23 +1,29 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from 'react-native';
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "react-native";
+
+// for redux setup globally
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 
 export default function PagesLayout() {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    return (
-        <Stack
-            screenOptions={{
-                title: '',
-                headerStyle: {
-                    backgroundColor: Colors[colorScheme ?? 'light'].background,
-                },
-                headerTintColor: Colors[colorScheme ?? 'light'].text,
-            }}
-        >
-            <Stack.Screen name='index' options={{ headerShown: false }} />
-            {/* You can add more screens and options here if needed */}
-        </Stack>
-    );
+  return (
+    <Provider store={store}>
+      <Stack
+        screenOptions={{
+          title: "",
+          headerStyle: {
+            backgroundColor: Colors[colorScheme ?? "light"].background,
+          },
+          headerTintColor: Colors[colorScheme ?? "light"].text,
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* You can add more screens and options here if needed */}
+      </Stack>
+    </Provider>
+  );
 }
