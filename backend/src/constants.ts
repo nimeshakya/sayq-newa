475f89ad189032b6dd76dd06d70b8292067ad89d
nimeshakya <nimeshakyabtw@gmail.com>
2025-09-19 21:10:06 +0545

    Get token from google after sign in and send back signed token with user details

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -3,3 +3,5 @@ dotenv.config();
 
 export const MONGO_URI = process.env.MONGO_URI;
 export const PORT = process.env.PORT;
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const JWT_SECRET = process.env.JWT_SECRET;
