c2b57cc66e29247c33c471e7f41613ce56480225
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-09-20 12:54:43 +0545

    setting up for option context

--- frontend/app/(pages)/_layout.tsx
+++ frontend/app/(pages)/_layout.tsx
@@ -1,23 +1,27 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from 'react-native';
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "react-native";
+
+import { OptionProvider } from "@/context/optionContext";
 
 export default function PagesLayout() {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    return (
-        <Stack
-            screenOptions={{
-                title: '',
-                headerStyle: {
-                    backgroundColor: Colors[colorScheme ?? 'light'].background,
-                },
-                headerTintColor: Colors[colorScheme ?? 'light'].text,
-            }}
-        >
-            <Stack.Screen name='index' options={{ headerShown: false }} />
-            {/* You can add more screens and options here if needed */}
-        </Stack>
-    );
+  return (
+    <OptionProvider>
+      <Stack
+        screenOptions={{
+          title: "",
+          headerStyle: {
+            backgroundColor: Colors[colorScheme ?? "light"].background,
+          },
+          headerTintColor: Colors[colorScheme ?? "light"].text,
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* You can add more screens and options here if needed */}
+      </Stack>
+    </OptionProvider>
+  );
 }
