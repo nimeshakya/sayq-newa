892c237cef6e162e1e260968d09d170ba22f643f
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-10-28 20:48:02 +0545

    created page and component for MCQ

---
+++ frontend/app/(pages)/questionPage.tsx
@@ -0,0 +1,66 @@
+import { Base } from "@/components/custom/base";
+import { StyleSheet, Text, View } from "react-native";
+import { useOption } from "@/context/optionContext";
+import { useMCQContext } from "@/context/MCQContext";
+import { MCQ } from "@/components/questions/MCQ";
+import { useEffect } from "react";
+
+export default function QuestionPage() {
+  const options = [
+    {
+      id: "1",
+      question:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe in quis velit exercitationem sed hic error quibusdam explicabo, atque sint natus sunt rem voluptas iste provident, labore vitae veritatis aut.",
+      answers: ["a", "b", "c"],
+      correct: "a",
+    },
+    {
+      id: "2",
+      question:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe in quis velit exercitationem sed hic error quibusdam explicabo, atque sint natus sunt rem voluptas iste provident, labore vitae veritatis aut.",
+      answers: ["a", "b", "c"],
+      correct: "a",
+    },
+    {
+      id: "3",
+      question:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe in quis velit exercitationem sed hic error quibusdam explicabo, atque sint natus sunt rem voluptas iste provident, labore vitae veritatis aut.",
+      answers: ["a", "b", "c"],
+      correct: "a",
+    },
+  ];
+
+  const { setTest, clearTest } = useMCQContext();
+
+  useEffect(() => {
+    clearTest();
+    setTest(options);
+  }, []);
+
+  return (
+    <Base>
+      {({ theme, colorScheme, styles: baseStyle }) => {
+        const styles = createStyle(theme);
+        return (
+          <>
+            <View style={styles.baseContainer}>
+              <MCQ />
+            </View>
+          </>
+        );
+      }}
+    </Base>
+  );
+}
+function createStyle(theme: { text: string; background: string }) {
+  return StyleSheet.create({
+    baseContainer: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    textStyle: {
+      color: theme.text,
+    },
+  });
+}
