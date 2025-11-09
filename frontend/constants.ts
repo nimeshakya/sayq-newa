ca7a551e97ec96adf93e54c453eea6f493b1cf58
nimeshakya <nimeshakyabtw@gmail.com>
2025-11-09 20:12:51 +0545

    Remove RN frontend

--- frontend/constants.ts
+++
@@ -1,4 +0,0 @@
-export const EXPO_PUBLIC_GOOGLE_CLIENT_ID =
-    process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID!;
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
-export const API_BASE_URL = `${process.env.EXPO_PUBLIC_API_BASE_URL!}/api`;
