9223b045088d7123e1b817583ea3dc9c4da1c48e
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-12-06 15:53:17 +0545

    adding context api for user activities

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/user.context";
 import "./index.scss";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
