c831f514422d2a24b37a20cc40ce7d8af9152f41
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-12-17 21:52:53 +0545

    indentation auto correct

--- backend/src/controllers/sessioncheck.controller.ts
+++ backend/src/controllers/sessioncheck.controller.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import UserModel from '../models/user.model';
-import { JWT_SECRET } from '../constants';
+import express from "express";
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user.model";
+import { JWT_SECRET } from "../constants";
 
 export const sessionCheckController = async (
-    req: express.Request,
-    res: express.Response
+  req: express.Request,
+  res: express.Response
 ) => {
-    try {
-        const googleid = (req as any).user?.sub;
-        const user = await UserModel.findOne({ googleId: googleid });
-        if (!user) return res.status(404).json({ message: 'User not found!' });
-        res.status(200).json({ user, message: 'User is authenticated!' });
-    } catch (error) {
-        res.status(500).json({ message: 'Session check failed!' }).end();
-    }
+  try {
+    const googleid = (req as any).user?.sub;
+    const user = await UserModel.findOne({ googleId: googleid });
+    if (!user) return res.status(404).json({ message: "User not found!" });
+    res.status(200).json({ user, message: "User is authenticated!" });
+  } catch (error) {
+    res.status(500).json({ message: "Session check failed!" }).end();
+  }
 };
--- backend/src/middlewares/authentication.middleware.ts
+++ backend/src/middlewares/authentication.middleware.ts
@@ -1,42 +1,39 @@
-import express from 'express';
+import express from "express";
 
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../constants';
-import { UserType } from '../db/user.schema';
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../constants";
+import { UserType } from "../db/user.schema";
 
 interface AuthenticatedRequest extends express.Request {
-    user?: UserType | string | jwt.JwtPayload;
+  user?: UserType | string | jwt.JwtPayload;
 }
 
 export const hasAuthenticationToken = (
-    req: AuthenticatedRequest,
-    res: express.Response,
-    next: express.NextFunction
+  req: AuthenticatedRequest,
+  res: express.Response,
+  next: express.NextFunction
 ) => {
-    try {
-        if (!JWT_SECRET) {
-            // Fail fast if secret is not set
-            throw new Error(
-                'JWT_SECRET is not defined in environment variables'
-            );
-        }
-
-        const token = req.cookies.token; // read the token in cookie
-        if (!token) return res.status(401).json({ message: 'No token!' });
-
-        // verify jwt
-        const decoded = jwt.verify(token, JWT_SECRET);
-
-        if (!decoded)
-            return res.status(401).json({ message: 'Invalid token!' });
-        req.user = decoded;
-        console.log(decoded);
-
-        next();
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ message: 'Authentication Failed!', error })
-            .end();
+  try {
+    if (!JWT_SECRET) {
+      // Fail fast if secret is not set
+      throw new Error("JWT_SECRET is not defined in environment variables");
     }
+
+    const token = req.cookies.token; // read the token in cookie
+    if (!token) return res.status(401).json({ message: "No token!" });
+
+    // verify jwt
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (!decoded) return res.status(401).json({ message: "Invalid token!" });
+    req.user = decoded;
+    console.log(decoded);
+
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Authentication Failed!", error })
+      .end();
+  }
 };
