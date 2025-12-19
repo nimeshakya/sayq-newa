a893b6705743bca78d8e164200e62c3d0e0656da
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-12-19 19:40:39 +0545

    using question generation api

--- frontend/src/pages/initialQuestion.tsx
+++ frontend/src/pages/initialQuestion.tsx
@@ -1,48 +1,11 @@
-import { useEffect } from "react";
 import "../styles/question.style.scss";
 import "../styles/_shared.scss";
-import { useQuestionContext } from "../context/question.context";
 import Question from "../components/questions.components";
 
 export default function InitialQuestion() {
-  const { setQuestions } = useQuestionContext();
-  const questions = [
-    {
-      id: 1,
-      question: "hellow 1 question?",
-      sub_question: undefined,
-      correct_answer: "a",
-      options: ["a", "s", "d", "f"],
-    },
-    {
-      id: 2,
-      question: "hellow  question what bnext?",
-      sub_question: undefined,
-      correct_answer: "a",
-      options: ["a", "s", "d", "f"],
-    },
-    {
-      id: 3,
-      question: "hellow  question?",
-      sub_question: undefined,
-      correct_answer: "a",
-      options: ["a", "s", "d", "f"],
-    },
-    {
-      id: 4,
-      question: "hellow  question?",
-      sub_question: undefined,
-      correct_answer: "a",
-      options: ["a", "s", "d", "f"],
-    },
-  ];
-  useEffect(() => {
-    setQuestions(questions);
-  }, []);
-
   return (
     <div className="main-centered-container">
-      <Question model_type="mcq" />
+      <Question headingDisplay="mcq" count={4} />
     </div>
   );
 }
