f808dcd9510e59f62d600e5e787b017fec025194
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-12-23 10:28:25 +0545

    route for pulling results from mongo to json

--- backend/src/router/userResultStore.route.ts
+++ backend/src/router/userResultStore.route.ts
@@ -1,6 +1,10 @@
 import express from "express";
-import { saveUserResult } from "../controllers/userResultStore.controller";
+import {
+  saveUserResult,
+  pullResultsFromMongoToJSON,
+} from "../controllers/userResultStore.controller";
 
 export default (router: express.Router): void => {
   router.post("/user-stat", saveUserResult);
+  router.get("/user-stat/sync", pullResultsFromMongoToJSON);
 };
