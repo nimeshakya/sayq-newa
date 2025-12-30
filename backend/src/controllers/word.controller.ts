5362648220c7644d23bda0e0db6b269a06193ba5
Saurav Dhoju <sauravdhoju12@gmail.com>
2025-12-30 23:16:49 +0545

    added another api for the fetching expertise level

--- backend/src/controllers/word.controller.ts
+++ backend/src/controllers/word.controller.ts
@@ -132,3 +132,13 @@ export const fetchCategories = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const fetchExpertiseLevel = async (req: Request, res: Response) => {
+  try {
+    const expertise_levels = await Word.distinct("expertise_lvl");
+    res.status(200).json(expertise_levels);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/src/router/word.router.ts
+++ backend/src/router/word.router.ts
@@ -4,6 +4,7 @@ import {
   fetchAllWords,
   fetchCategories,
   fetchDataWord,
+  fetchExpertiseLevel,
 } from "../controllers/word.controller";
 import { hasAuthenticationToken } from "../middlewares/authentication.middleware";
 
@@ -11,5 +12,6 @@ export default (router: Router) => {
   router.post("/words", addWord);
   router.get("/words/all", fetchAllWords);
   router.get("/words/categories", fetchCategories);
+  router.get("/words/expertise-level", fetchExpertiseLevel);
   router.get("/words", hasAuthenticationToken, fetchDataWord);
 };
